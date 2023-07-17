b47c52fefe44be9262eed7a395c6be8577e34b19
micheal00000 <michealhany991@gmail.com>
2023-07-17 10:33:55 +0300

    take input form User and valdate it

--- lib/views/add-note-bottom-sheet.dart
+++ lib/views/add-note-bottom-sheet.dart
@@ -9,20 +9,95 @@ class AddNoteShowModelButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        child:const Column(
-          children: [
-            CustomTextFled(hint: 'Title', maxLines: 1),
-            CustomTextFled(hint: 'Content', maxLines: 5),
-            SizedBox(
-              height: 30,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 10,
-            )
-          ],
-        ),
+        child: const addNoteForm(),
       ),
     );
   }
 }
+
+class addNoteForm extends StatefulWidget {
+  const addNoteForm({
+    super.key,
+  });
+
+  @override
+  State<addNoteForm> createState() => _addNoteFormState();
+}
+
+class _addNoteFormState extends State<addNoteForm> {
+  final formkey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          CustomTextFled(
+            hint: 'Title',
+            maxLines: 1,
+            onSaved: (p0) {
+              title = p0;
+            },
+          ),
+          CustomTextFled(
+            hint: 'Content',
+            maxLines: 5,
+            onSaved: (p0) {
+              subTitle = p0;
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+// class addNoteForm extends StatefulWidget {
+//   const addNoteForm({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   State<addNoteForm> createState() => _addNoteFormState();
+// }
+
+// class _addNoteFormState extends State<addNoteForm> {
+//   final formkey = GlobalKey<FormState>(); // Initialize formkey with a new instance of GlobalKey<FormState>
+//   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+//   String? title, subTitle;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Form(
+//       key: formkey, // Set the formkey to the Form widget
+//       autovalidateMode: autovalidateMode,
+//       child: Column(
+//         children: [
+//           // ...
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/views/widgets/custom-text-field.dart
+++ lib/views/widgets/custom-text-field.dart
@@ -2,21 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:note_app_with_hive/constants.dart';
 
 class CustomTextFled extends StatelessWidget {
-  const CustomTextFled({required this.hint, required this.maxLines});
+  const CustomTextFled(
+      {required this.hint, required this.maxLines, this.onSaved});
   final String hint;
   final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-      child: TextField(
+      child: TextFormField(
+        validator: (value) {
+          if (value?.isEmpty ?? true) {
+            return ' Filed id required';
+          }
+          return null;
+        },
+        onSaved: onSaved,
         maxLines: maxLines,
-        cursorColor: Kprimarycolor,
+        cursorColor: kprimarycolor,
         decoration: InputDecoration(
             hintText: hint,
-            hintStyle: const TextStyle(color: Kprimarycolor, fontSize: 18),
+            hintStyle: const TextStyle(color: kprimarycolor, fontSize: 18),
             enabledBorder: builedBorder(),
-            focusedBorder: builedBorder(Kprimarycolor)),
+            errorBorder: builedBorder(),
+            border: builedBorder(),
+            focusedBorder: builedBorder(kprimarycolor)),
       ),
     );
   }
@@ -24,7 +35,7 @@ class CustomTextFled extends StatelessWidget {
   OutlineInputBorder builedBorder([color]) {
     return OutlineInputBorder(
         borderSide: BorderSide(color: color ?? Colors.white),
-        borderRadius: BorderRadius.all(
+        borderRadius: const BorderRadius.all(
           Radius.circular(8),
         ));
   }

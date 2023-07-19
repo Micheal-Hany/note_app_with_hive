3ae56e4bdd92279ed0789d82f9ff048c283dd7fb
micheal00000 <michealhany991@gmail.com>
2023-07-19 15:11:48 +0300

    End of app

--- lib/views/widgets/note-list-view.dart
+++ lib/views/widgets/note-list-view.dart
@@ -9,7 +9,6 @@ import 'package:note_app_with_hive/views/widgets/custom-note-item.dart';
 class NoteListView extends StatelessWidget {
   const NoteListView({super.key});
 
-  @override
   Widget build(BuildContext context) {
     return BlocBuilder<NotesCubit, NotesState>(builder: (context, state) {
       List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
